9a1d5b14736efb87e91d0fb705a31aa4b813ab77
Jude <judecapachietti@gmail.com>
2024-06-30 05:27:14 +0000

    remove opengl stuff and put simple SDL2 example code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
 /*
 TO DO:
-  - Get rid of the need for  `-sLEGACY_GL_EMULATION` in compiler
-    - Cut out starter code and replace it with simple SDL2 things
   - Move something around the screen with mouse or keypresses
   - Simplify code (input and otherwise)
   - Ensure assets can be loaded in webpage
@@ -9,110 +7,18 @@ TO DO:
 */
 
 #include "SDL2/SDL.h"
-#include "SDL2/SDL_opengl.h"
 
 #include <stdio.h>
-#include <string.h>
 #include <assert.h>
 
 #include <emscripten.h>
 #include <emscripten/key_codes.h>
 #include <emscripten/html5.h>
-#include <stdio.h>
 
 SDL_Window *window;
-
-// GL_ARB_shading_language_100, GL_ARB_shader_objects, GL_ARB_fragment_shader, GL_ARB_vertex_shader
-PFNGLCREATEPROGRAMOBJECTARBPROC       glCreateProgramObject_      = NULL;
-PFNGLDELETEOBJECTARBPROC              glDeleteObject_             = NULL;
-PFNGLUSEPROGRAMOBJECTARBPROC          glUseProgramObject_         = NULL; 
-PFNGLCREATESHADEROBJECTARBPROC        glCreateShaderObject_       = NULL;
-PFNGLSHADERSOURCEARBPROC              glShaderSource_             = NULL;
-PFNGLCOMPILESHADERARBPROC             glCompileShader_            = NULL;
-PFNGLGETOBJECTPARAMETERIVARBPROC      glGetObjectParameteriv_     = NULL;
-PFNGLATTACHOBJECTARBPROC              glAttachObject_             = NULL;
-PFNGLGETINFOLOGARBPROC                glGetInfoLog_               = NULL;
-PFNGLLINKPROGRAMARBPROC               glLinkProgram_              = NULL;
-PFNGLGETUNIFORMLOCATIONARBPROC        glGetUniformLocation_       = NULL;
-PFNGLUNIFORM1FARBPROC                 glUniform1f_                = NULL;
-PFNGLUNIFORM2FARBPROC                 glUniform2f_                = NULL;
-PFNGLUNIFORM3FARBPROC                 glUniform3f_                = NULL;
-PFNGLUNIFORM4FARBPROC                 glUniform4f_                = NULL;
-PFNGLUNIFORM1FVARBPROC                glUniform1fv_               = NULL;
-PFNGLUNIFORM2FVARBPROC                glUniform2fv_               = NULL;
-PFNGLUNIFORM3FVARBPROC                glUniform3fv_               = NULL;
-PFNGLUNIFORM4FVARBPROC                glUniform4fv_               = NULL;
-PFNGLUNIFORM1IARBPROC                 glUniform1i_                = NULL;
-PFNGLBINDATTRIBLOCATIONARBPROC        glBindAttribLocation_       = NULL;
-PFNGLGETACTIVEUNIFORMARBPROC          glGetActiveUniform_         = NULL;
-
-void initARB() {
-  glCreateProgramObject_ =        (PFNGLCREATEPROGRAMOBJECTARBPROC)     SDL_GL_GetProcAddress("glCreateProgramObjectARB");
-  glDeleteObject_ =               (PFNGLDELETEOBJECTARBPROC)            SDL_GL_GetProcAddress("glDeleteObjectARB");
-  glUseProgramObject_ =           (PFNGLUSEPROGRAMOBJECTARBPROC)        SDL_GL_GetProcAddress("glUseProgramObjectARB");
-  glCreateShaderObject_ =         (PFNGLCREATESHADEROBJECTARBPROC)      SDL_GL_GetProcAddress("glCreateShaderObjectARB");
-  glShaderSource_ =               (PFNGLSHADERSOURCEARBPROC)            SDL_GL_GetProcAddress("glShaderSourceARB");
-  glCompileShader_ =              (PFNGLCOMPILESHADERARBPROC)           SDL_GL_GetProcAddress("glCompileShaderARB");
-  glGetObjectParameteriv_ =       (PFNGLGETOBJECTPARAMETERIVARBPROC)    SDL_GL_GetProcAddress("glGetObjectParameterivARB");
-  glAttachObject_ =               (PFNGLATTACHOBJECTARBPROC)            SDL_GL_GetProcAddress("glAttachObjectARB");
-  glGetInfoLog_ =                 (PFNGLGETINFOLOGARBPROC)              SDL_GL_GetProcAddress("glGetInfoLogARB");
-  glLinkProgram_ =                (PFNGLLINKPROGRAMARBPROC)             SDL_GL_GetProcAddress("glLinkProgramARB");
-  glGetUniformLocation_ =         (PFNGLGETUNIFORMLOCATIONARBPROC)      SDL_GL_GetProcAddress("glGetUniformLocationARB");
-  glUniform1f_ =                  (PFNGLUNIFORM1FARBPROC)               SDL_GL_GetProcAddress("glUniform1fARB");
-  glUniform2f_ =                  (PFNGLUNIFORM2FARBPROC)               SDL_GL_GetProcAddress("glUniform2fARB");
-  glUniform3f_ =                  (PFNGLUNIFORM3FARBPROC)               SDL_GL_GetProcAddress("glUniform3fARB");
-  glUniform4f_ =                  (PFNGLUNIFORM4FARBPROC)               SDL_GL_GetProcAddress("glUniform4fARB");
-  glUniform1fv_ =                 (PFNGLUNIFORM1FVARBPROC)              SDL_GL_GetProcAddress("glUniform1fvARB");
-  glUniform2fv_ =                 (PFNGLUNIFORM2FVARBPROC)              SDL_GL_GetProcAddress("glUniform2fvARB");
-  glUniform3fv_ =                 (PFNGLUNIFORM3FVARBPROC)              SDL_GL_GetProcAddress("glUniform3fvARB");
-  glUniform4fv_ =                 (PFNGLUNIFORM4FVARBPROC)              SDL_GL_GetProcAddress("glUniform4fvARB");
-  glUniform1i_ =                  (PFNGLUNIFORM1IARBPROC)               SDL_GL_GetProcAddress("glUniform1iARB");
-  glBindAttribLocation_ =         (PFNGLBINDATTRIBLOCATIONARBPROC)      SDL_GL_GetProcAddress("glBindAttribLocationARB");
-  glGetActiveUniform_ =           (PFNGLGETACTIVEUNIFORMARBPROC)        SDL_GL_GetProcAddress("glGetActiveUniformARB");
-}
-
-void setShaders() {
-  GLuint v, f, p;
-  GLint ok;
-
-	const char *vv = "void main()                   \n"
-                   "{                             \n"
-	                 "  gl_Position = ftransform() + vec4(0.1, -0.25, 0, 0); \n"
-                   "}";
-  const char *ff = "void main()                \n"
-                   "{	                         \n"
-	                 "  gl_FragColor = vec4(gl_FragCoord.y/480.0, gl_FragCoord.x/640.0, 0.66, 1.0); \n"
-                   "}";
-
-	v = glCreateShaderObject_(GL_VERTEX_SHADER);
-	f = glCreateShaderObject_(GL_FRAGMENT_SHADER);
-
-	glShaderSource_(v, 1, &vv,NULL);
-	glShaderSource_(f, 1, &ff,NULL);
-
-	glCompileShader_(v);
-  glGetObjectParameteriv_(v, GL_OBJECT_COMPILE_STATUS_ARB, &ok);
-  if (!ok) {
-    char msg[512];
-    glGetInfoLog_(v, sizeof msg, NULL, msg);
-    printf("shader compilation issue: %s\n", msg);
-  }
-  assert(ok);
-
-	glCompileShader_(f);
-  glGetObjectParameteriv_(f, GL_OBJECT_COMPILE_STATUS_ARB, &ok);
-  assert(ok);
-
-	p = glCreateProgramObject_();
-	glAttachObject_(p,f);
-	glAttachObject_(p,v);
-
-	glLinkProgram_(p);
-  glGetObjectParameteriv_(p, GL_OBJECT_LINK_STATUS_ARB, &ok);
-  assert(ok);
-
-	glUseProgramObject_(p);
-}
+SDL_Renderer *renderer;
+SDL_Surface *screen;
+SDL_Texture *screenTexture;
 
 static inline const char *emscripten_event_type_to_string(int eventType) {
   const char *events[] = { "(invalid)", "(none)", "keypress", "keydown", "keyup", "click", "mousedown", "mouseup", "dblclick", "mousemove", "wheel", "resize", 
@@ -169,60 +75,77 @@ EM_BOOL key_callback(int eventType, const EmscriptenKeyboardEvent *e, void *user
 // Our "main loop" function. This callback receives the current time as
 // reported by the browser, and the user data we provide in the call to
 // emscripten_request_animation_frame_loop().
+// Docs: https://emscripten.org/docs/porting/emscripten-runtime-environment.html#implementing-an-asynchronous-main-loop-in-c-c
 EM_BOOL one_iter(double time, void* userData) {
   // Can render to the screen here, etc.
-  puts("one iteration");
-    glColor3f(0, 1, 1); // is overridden by the shader, useful for debugging native builds
-    glBegin( GL_TRIANGLES );
-      glTexCoord2i(0, 0); glVertex3f( 10,  10,  0);
-      glTexCoord2i(1, 0); glVertex3f( 300, 10,  0);
-      glTexCoord2i(1, 1); glVertex3f( 300, 328, 0);
-    glEnd();
-
-    glColor3f(1, 1, 0); // is overridden by the shader, useful for debugging native builds
-    glBegin( GL_TRIANGLES );
-        glTexCoord2f(0, 0.5); glVertex3f(410, 10,  0);
-        glTexCoord2f(1, 0.5); glVertex3f(600, 10,  0);
-        glTexCoord2f(1, 1  ); glVertex3f(630, 400, 0);
-    glEnd();
-
-    SDL_GL_SwapWindow(window);
+  SDL_RenderClear(renderer);
+  SDL_RenderCopy(renderer, screenTexture, NULL, NULL);
+  SDL_RenderPresent(renderer);
 
   // Return true to keep the loop running.
   return EM_TRUE;
 }
 
 int main() {
-      //SDL_Surface *screen;
-    
-    SDL_GLContext context;
-    emscripten_set_keydown_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW, 0, 1, key_callback);
-    emscripten_set_click_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW, 0, 1, mouse_callback);
+  // Code adapted from `./include/emsdk/upstream/emscripten/test/browser/test_sdl2_canvas_palette.c`
+  assert(SDL_Init(SDL_INIT_VIDEO) == 0);
 
+  SDL_CreateWindowAndRenderer(600, 400, 0, &window, &renderer);
 
-    assert(SDL_Init(SDL_INIT_VIDEO) == 0);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    //screen = SDL_SetVideoMode( 640, 480, 16, SDL_OPENGL );
-    //assert(screen);
+  screen = SDL_CreateRGBSurface(0, 600, 400, 8, 0, 0, 0, 0);
 
-    window = SDL_CreateWindow("sdlglshader", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_OPENGL);
-    assert(window);
+  // These might have to go after like setting up the screen or window something:
+  emscripten_set_keydown_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW, 0, 1, key_callback);
+  emscripten_set_click_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW, 0, 1, mouse_callback);
 
-    context = SDL_GL_CreateContext(window);
+  //initialize sdl palette
+  //with red green and blue
+  //colors
+  SDL_Color pal[4];
+  pal[0].r = 255;
+  pal[0].g = 0;
+  pal[0].b = 0;
+  pal[0].a = 255;
 
-    glClearColor(0, 0, 0, 0);
-    glViewport(0, 0, 640, 480);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(0, 640, 480, 0, -1, 1);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    glClear(GL_COLOR_BUFFER_BIT);
+  pal[1].r = 0;
+  pal[1].g = 255;
+  pal[1].b = 0;
+  pal[1].a = 255;
 
-    initARB();
-    setShaders();
+  pal[2].r = 0;
+  pal[2].g = 0;
+  pal[2].b = 255;
+  pal[2].a = 255;
+
+  pal[3].r = 255;
+  pal[3].g = 255;
+  pal[3].b = 0;
+  pal[3].a = 255;
+
+  SDL_SetPaletteColors(screen->format->palette, pal, 0, 4);
+
+  SDL_FillRect(screen, NULL, 0);
+
+  {
+    SDL_Rect rect = { 300, 0, 300, 200 };
+    SDL_FillRect(screen, &rect, 1);
+  }
+
+  {
+    SDL_Rect rect = { 0, 200, 600, 200 };
+    SDL_FillRect(screen, &rect, 2);
+  }
+
+  {
+    SDL_Rect rect = { 300, 200, 300, 200 };
+    SDL_FillRect(screen, &rect, 3);
+  }
+
+  screenTexture = SDL_CreateTextureFromSurface(renderer, screen);
+
 #ifdef __EMSCRIPTEN__
   // Receives a function to call and some user data to provide it.
+  SDL_Delay(3000);  // <-- Might not need this.  There might be specific loader function.
   emscripten_request_animation_frame_loop(one_iter, 0);
 
 // Not sure how to make this run at the moment:
