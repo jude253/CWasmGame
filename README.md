691c650724fbde4c4266e1e2be8111be2901360a
Jude <judecapachietti@gmail.com>
2024-06-30 04:47:26 +0000

    add infinite loop and user input

--- README.md
+++ README.md
@@ -2,21 +2,24 @@
 
 This is a simple project to build a game for Wasm using Emscripten and CMake.  
 
-## Project Setup Resources
-
-The project configuration was set up using these resources:
-- CMake docs: https://cmake.org/
-- CMake installation: https://github.com/Kitware/CMake?tab=readme-ov-file#unixmac-osxmingwmsyscygwin
-- https://github.com/microsoft/vscode-cmake-tools/blob/531c8d3bf06f243095ffa3a3760239258b4ccb8f/docs/README.md
-- https://code.visualstudio.com/docs/cpp/CMake-linux
-  - https://youtu.be/_BWU5mWqVA4ß
-- Emscripten docs: https://emscripten.org/
+## Dependencies:
+- CMake
+  - Docs: https://cmake.org/
+  - Installation: https://github.com/Kitware/CMake?tab=readme-ov-file#unixmac-osxmingwmsyscygwin
+- Emscripten
+  - Docs: https://emscripten.org/
+  - Installation: https://emscripten.org/docs/getting_started/downloads.html
 
 ## Build Commands
 
 #### Initial Project Setup
 
+
 ```
+# Clone repo and submodules:
+git clone --recurse-submodules https://github.com/jude253/CWasmGame.git
+
+# Setup up emscripten
 ./setup_emscripten.sh
 
 # Run this on initial setup and after adjusting build scripts:
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,11 @@
 /*
- * Copyright 2014 The Emscripten Authors.  All rights reserved.
- * Emscripten is available under two separate licenses, the MIT license and the
- * University of Illinois/NCSA Open Source License.  Both these licenses can be
- * found in the LICENSE file.
- */
-
-/*
-THIS WORK, INCLUDING THE SOURCE CODE, DOCUMENTATION
-AND RELATED MEDIA AND DATA, IS PLACED INTO THE PUBLIC DOMAIN.
-
-THE ORIGINAL AUTHOR IS KYLE FOLEY.
-
-THIS SOFTWARE IS PROVIDED AS-IS WITHOUT WARRANTY
-OF ANY KIND, NOT EVEN THE IMPLIED WARRANTY OF
-MERCHANTABILITY. THE AUTHOR OF THIS SOFTWARE,
-ASSUMES _NO_ RESPONSIBILITY FOR ANY CONSEQUENCE
-RESULTING FROM THE USE, MODIFICATION, OR
-REDISTRIBUTION OF THIS SOFTWARE.
+TO DO:
+  - Get rid of the need for  `-sLEGACY_GL_EMULATION` in compiler
+    - Cut out starter code and replace it with simple SDL2 things
+  - Move something around the screen with mouse or keypresses
+  - Simplify code (input and otherwise)
+  - Ensure assets can be loaded in webpage
+  - Maybe break out code into different files
 */
 
 #include "SDL2/SDL.h"
@@ -25,6 +14,13 @@ REDISTRIBUTION OF THIS SOFTWARE.
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+
+#include <emscripten.h>
+#include <emscripten/key_codes.h>
+#include <emscripten/html5.h>
+#include <stdio.h>
+
+SDL_Window *window;
 
 // GL_ARB_shading_language_100, GL_ARB_shader_objects, GL_ARB_fragment_shader, GL_ARB_vertex_shader
 PFNGLCREATEPROGRAMOBJECTARBPROC       glCreateProgramObject_      = NULL;
@@ -118,11 +114,91 @@ void setShaders() {
 	glUseProgramObject_(p);
 }
 
-int main(int argc, char *argv[])
+static inline const char *emscripten_event_type_to_string(int eventType) {
+  const char *events[] = { "(invalid)", "(none)", "keypress", "keydown", "keyup", "click", "mousedown", "mouseup", "dblclick", "mousemove", "wheel", "resize", 
+    "scroll", "blur", "focus", "focusin", "focusout", "deviceorientation", "devicemotion", "orientationchange", "fullscreenchange", "pointerlockchange", 
+    "visibilitychange", "touchstart", "touchend", "touchmove", "touchcancel", "gamepadconnected", "gamepaddisconnected", "beforeunload", 
+    "batterychargingchange", "batterylevelchange", "webglcontextlost", "webglcontextrestored", "mouseenter", "mouseleave", "mouseover", "mouseout", "(invalid)" };
+  ++eventType;
+  if (eventType < 0) eventType = 0;
+  if (eventType >= sizeof(events)/sizeof(events[0])) eventType = sizeof(events)/sizeof(events[0])-1;
+  return events[eventType];
+}
+
+EM_BOOL mouse_callback(int eventType, const EmscriptenMouseEvent *e, void *userData) {
+  printf("%s, screen: (%d,%d), client: (%d,%d),%s%s%s%s button: %hu, buttons: %hu, movement: (%d,%d), target: (%d, %d)\n",
+    emscripten_event_type_to_string(eventType), e->screenX, e->screenY, e->clientX, e->clientY,
+    e->ctrlKey ? " CTRL" : "", e->shiftKey ? " SHIFT" : "", e->altKey ? " ALT" : "", e->metaKey ? " META" : "", 
+    e->button, e->buttons, e->movementX, e->movementY, e->targetX, e->targetY);
+
+  if (e->screenX != 0 && e->screenY != 0 && e->clientX != 0 && e->clientY != 0 && e->targetX != 0 && e->targetY != 0) {
+    if (eventType == EMSCRIPTEN_EVENT_CLICK) printf("gotClick = 1\n");
+    if (eventType == EMSCRIPTEN_EVENT_MOUSEDOWN && e->buttons != 0) printf("gotClick = 1\n");
+    if (eventType == EMSCRIPTEN_EVENT_DBLCLICK) printf("gotClick = 1\n");
+    if (eventType == EMSCRIPTEN_EVENT_MOUSEUP) printf("gotClick = 1\n");
+    if (eventType == EMSCRIPTEN_EVENT_MOUSEMOVE && (e->movementX != 0 || e->movementY != 0)) printf("gotMouseMove = 1\n");
+  }
+  // draw();
+
+  return 0;
+}
+
+
+EM_BOOL key_callback(int eventType, const EmscriptenKeyboardEvent *e, void *userData)
 {
-    //SDL_Surface *screen;
-    SDL_Window *window;
+  int dom_pk_code = emscripten_compute_dom_pk_code(e->code);
+
+  printf("%s, key: \"%s\" \n",
+    emscripten_event_type_to_string(eventType), 
+    e->key
+  );
+  if (eventType == EMSCRIPTEN_EVENT_KEYUP) printf("\n"); // Visual cue
+
+  // draw();
+  // Return true for events we want to suppress default web browser handling for.
+  // For testing purposes, want to return false here on most KeyDown messages so that they get transformed to KeyPress messages.
+  return e->keyCode == DOM_VK_BACK_SPACE // Don't navigate away from this test page on backspace.
+    || e->keyCode == DOM_VK_TAB // Don't change keyboard focus to different browser UI elements while testing.
+    || (e->keyCode >= DOM_VK_F1 && e->keyCode <= DOM_VK_F24) // Don't F5 refresh the test page to reload.
+    || e->ctrlKey // Don't trigger e.g. Ctrl-B to open bookmarks
+    || e->altKey // Don't trigger any alt-X based shortcuts either (Alt-F4 is not overrideable though)
+    || eventType == EMSCRIPTEN_EVENT_KEYPRESS || eventType == EMSCRIPTEN_EVENT_KEYUP; // Don't perform any default actions on these.
+}
+
+
+// Our "main loop" function. This callback receives the current time as
+// reported by the browser, and the user data we provide in the call to
+// emscripten_request_animation_frame_loop().
+EM_BOOL one_iter(double time, void* userData) {
+  // Can render to the screen here, etc.
+  puts("one iteration");
+    glColor3f(0, 1, 1); // is overridden by the shader, useful for debugging native builds
+    glBegin( GL_TRIANGLES );
+      glTexCoord2i(0, 0); glVertex3f( 10,  10,  0);
+      glTexCoord2i(1, 0); glVertex3f( 300, 10,  0);
+      glTexCoord2i(1, 1); glVertex3f( 300, 328, 0);
+    glEnd();
+
+    glColor3f(1, 1, 0); // is overridden by the shader, useful for debugging native builds
+    glBegin( GL_TRIANGLES );
+        glTexCoord2f(0, 0.5); glVertex3f(410, 10,  0);
+        glTexCoord2f(1, 0.5); glVertex3f(600, 10,  0);
+        glTexCoord2f(1, 1  ); glVertex3f(630, 400, 0);
+    glEnd();
+
+    SDL_GL_SwapWindow(window);
+
+  // Return true to keep the loop running.
+  return EM_TRUE;
+}
+
+int main() {
+      //SDL_Surface *screen;
+    
     SDL_GLContext context;
+    emscripten_set_keydown_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW, 0, 1, key_callback);
+    emscripten_set_click_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW, 0, 1, mouse_callback);
+
 
     assert(SDL_Init(SDL_INIT_VIDEO) == 0);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
@@ -145,28 +221,16 @@ int main(int argc, char *argv[])
 
     initARB();
     setShaders();
+#ifdef __EMSCRIPTEN__
+  // Receives a function to call and some user data to provide it.
+  emscripten_request_animation_frame_loop(one_iter, 0);
 
-    glColor3f(0, 1, 1); // is overridden by the shader, useful for debugging native builds
-    glBegin( GL_TRIANGLES );
-      glTexCoord2i(0, 0); glVertex3f( 10,  10,  0);
-      glTexCoord2i(1, 0); glVertex3f( 300, 10,  0);
-      glTexCoord2i(1, 1); glVertex3f( 300, 328, 0);
-    glEnd();
-
-    glColor3f(1, 1, 0); // is overridden by the shader, useful for debugging native builds
-    glBegin( GL_TRIANGLES );
-        glTexCoord2f(0, 0.5); glVertex3f(410, 10,  0);
-        glTexCoord2f(1, 0.5); glVertex3f(600, 10,  0);
-        glTexCoord2f(1, 1  ); glVertex3f(630, 400, 0);
-    glEnd();
-
-    SDL_GL_SwapWindow(window);
-
-#ifndef __EMSCRIPTEN__
-    SDL_Delay(3000);
+// Not sure how to make this run at the moment:
+// #else   
+//   while (1) {
+//     one_iter();
+//     // Delay to keep frame rate constant (using SDL).
+//     SDL_Delay(time_to_next_frame());
+//   }
 #endif
-
-    // Don't quit - we need to reftest the canvas! SDL_Quit();
-    return 0;
 }
-
